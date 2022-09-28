194b3553354ab4723aebcbea2c1c3906b5bd555f
João Gouveia <joao.gouveia2@pt.bosch.com>
2022-09-28 10:52:33 +0100

    improved performance on Special Pythagorean Triplet

--- src/main/kotlin/SpecialPythagoreanTriplet.kt
+++ src/main/kotlin/SpecialPythagoreanTriplet.kt
@@ -1,8 +1,28 @@
 import base.*
+import kotlin.math.pow
 
 class SpecialPythagoreanTriplet: Solution { 
-	override val rightSolution = 31875000L /* ENTER HERE THE SOLUTION RESULT */
+	override val rightSolution = 31875000L
 	override fun solve(): Long{
-		return 0L
+		var a = 0.0
+		var b = 0.0
+		var c = 0.0
+
+		var isCriteriaSatisfied = false
+
+		while (!isCriteriaSatisfied){
+			if(a < b) a++
+			else if (b < c){
+				a = 0.0
+				b++
+			} else{
+				b = 0.0
+				c++
+			}
+
+			isCriteriaSatisfied = (a + b + c) == 1000.0 && (a.pow(2.0) + b.pow(2.0) == c.pow(2.0))
+		}
+
+		return (a * b * c).toLong()
 	}
  }
