a3fb73ad0996e2e79c660c4f0151ca1db3338993
João Gouveia <joao.gouveia2@pt.bosch.com>
2022-10-02 17:26:23 +0100

    Long Grid class get function and getRange almost done

---
+++ src/main/kotlin/grid/LongGrid.kt
@@ -0,0 +1,36 @@
+package grid
+
+class LongGrid(private val lines: Int, private val columns: Int, private val elements: LongArray){
+
+    enum class Direction{
+        RIGHT, DOWN, DIAGONAL_DOWN, DIAGONAL_UP
+    }
+
+    companion object{
+        fun fromText(gridStr: String, numbersSeparatorCharacter: Char = ' '): LongGrid{
+            val splintedGrid = gridStr.split("\n").map { it.split(numbersSeparatorCharacter) }
+            return LongGrid(
+                splintedGrid.count(),
+                splintedGrid.first().size,
+                splintedGrid.flatten().map { it.toLong() }.toLongArray()
+            )
+        }
+    }
+
+    fun get(line: Int, column: Int) = elements[lines * line + column]
+
+    fun getRange(fromLine: Int,
+                 fromColumn: Int,
+                 rangeSize: Int,
+                 direction: Direction = Direction.RIGHT): LongArray =
+        // MISSING SOME INPUT VALIDATION HERE, WHAT IF THE RANGE IS BIGGER THAN THE NEIGHBOR ELEMENTS QUANTITY?
+            when(direction){
+                Direction.RIGHT -> elements.copyOfRange(lines * fromLine + fromColumn, rangeSize + 1)
+                // an = a1 + (n - 1) * r
+                Direction.DOWN -> (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * columns] }.toLongArray()
+                Direction.DIAGONAL_DOWN ->  (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * (columns + 1)] }.toLongArray()
+                //missing this one
+                Direction.DIAGONAL_UP -> (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * (columns + 1)] }.toLongArray()
+            }
+
+}
---
+++ src/test/kotlin/problems/LargestProductInAGridTest.kt
@@ -0,0 +1,8 @@
+package problems
+
+import LargestProductInAGrid
+import base.ProblemTest
+
+class LargestProductInAGridTest: ProblemTest() { 
+	override val solution = LargestProductInAGrid() 
+}
