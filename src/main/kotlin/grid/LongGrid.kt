50c8a3ee3cbd04eb38b0ae12f143401556ba972b
João Gouveia <joao.gouveia2@pt.bosch.com>
2022-10-13 21:23:44 +0100

    diagonal up ok, and added a validation to direction right, but it is missing to think a better way to create a validation function to each direction

--- src/main/kotlin/grid/LongGrid.kt
+++ src/main/kotlin/grid/LongGrid.kt
@@ -19,18 +19,23 @@ class LongGrid(private val lines: Int, private val columns: Int, private val ele
 
     fun get(line: Int, column: Int) = elements[lines * line + column]
 
+    private fun isValidRangeRight( from: Int, rangeSize: Int) =
+        from + rangeSize < columns
+
     fun getRange(fromLine: Int,
                  fromColumn: Int,
                  rangeSize: Int,
                  direction: Direction = Direction.RIGHT): LongArray =
         // MISSING SOME INPUT VALIDATION HERE, WHAT IF THE RANGE IS BIGGER THAN THE NEIGHBOR ELEMENTS QUANTITY?
             when(direction){
-                Direction.RIGHT -> elements.copyOfRange(lines * fromLine + fromColumn, rangeSize + 1)
+                Direction.RIGHT -> if(fromColumn + rangeSize >= columns)
+                    longArrayOf()
+                else
+                    elements.copyOfRange(lines * fromLine + fromColumn, lines * fromLine + fromColumn + rangeSize + 1)
                 // an = a1 + (n - 1) * r
                 Direction.DOWN -> (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * columns] }.toLongArray()
                 Direction.DIAGONAL_DOWN ->  (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * (columns + 1)] }.toLongArray()
-                //missing this one
-                Direction.DIAGONAL_UP -> (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * (columns + 1)] }.toLongArray()
+                Direction.DIAGONAL_UP -> (1 until rangeSize + 1).map { elements[(lines * fromLine + fromColumn) + (it - 1) * (columns - 1) * -1] }.toLongArray()
             }
 
 }
