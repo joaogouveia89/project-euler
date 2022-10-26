5610fe6ffa3c3e49e4bd1d848200be35bbd31c25
João Gouveia <joao.gouveia2@pt.bosch.com>
2022-10-26 13:25:56 +0100

    Large sum solved

--- src/main/kotlin/ext/StringExt.kt
+++ src/main/kotlin/ext/StringExt.kt
@@ -1,3 +1,8 @@
 package base
 
 fun String.isPalindrome() = this == this.reversed()
+
+fun String.getSubstring(fromIndex: Int, toIndex: Int) =
+    toCharArray()
+        .copyOfRange(fromIndex, toIndex)
+        .joinToString("")
---
+++ src/test/kotlin/problems/LargeSumTest.kt
@@ -0,0 +1,8 @@
+package problems
+
+import LargeSum
+import base.ProblemTest
+
+class LargeSumTest : ProblemTest() {
+    override val solution = LargeSum()
+}
