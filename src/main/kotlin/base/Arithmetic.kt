c9b25176da35c50184647a3adf4596a04665db93
João Gouveia <goj2lis@bosch.com>
2023-07-12 16:05:40 +0100

    interpolation funciton to arithmetic class

--- src/main/kotlin/base/Arithmetic.kt
+++ src/main/kotlin/base/Arithmetic.kt
@@ -5,6 +5,24 @@ open class Arithmetic {
         if(b == 0L)  a;
         else mdc(b, a % b);
 
+
+    // Thanks to https://www.geeksforgeeks.org/lagranges-interpolation/
+    fun interpolate(points: List<Pair<Int, Int>>, yn: Int): Int{
+        val n = points.size
+        var result = 0.0 // Initialize result
+        for (i in 0 until n) {
+            // Compute individual terms of above formula
+            var term: Double = points[i].second.toDouble()
+            for (j in 0 until n) {
+                if (j != i) term = term * (yn - points[j].first) / (points[i].first - points[j].first)
+            }
+
+            // Add current term to result
+            result += term
+        }
+        return result.toInt()
+    }
+
     // https://www.geeksforgeeks.org/multiplicative-order/
     fun multiplicativeOrder(a: Long, n: Long): Long{
         var result = 1L
