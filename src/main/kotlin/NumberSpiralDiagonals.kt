9d465dd00a721245cbbde618aed6e41174901a9f
João Gouveia <goj2lis@bosch.com>
2023-07-12 17:01:30 +0100

    improved the number of spiral diagonals

--- src/main/kotlin/NumberSpiralDiagonals.kt
+++ src/main/kotlin/NumberSpiralDiagonals.kt
@@ -2,35 +2,42 @@ import base.*
 import kotlinx.coroutines.*
 import kotlin.math.pow
 
-/* Interpolating the values on the diagonals I got these equations
-* 4 x^2 - 10 x + 7
-* 4 x^2 - 8 x + 5
-* 4 x^2 - 6 x + 3
-* 4 x^2 - 4 x + 1
+/*
+* The diagonals values grows up in a linear way, so I can interpolate the given values to
+* know the other points
  */
-class NumberSpiralDiagonals: Solution { 
+class NumberSpiralDiagonals: Solution, Arithmetic() {
 	override val rightSolution = 669171001L
 
 	private val scope = CoroutineScope(Dispatchers.IO + Job())
 
-	private val size = 501
+	private val size = 500
 
 	override fun solve(): Long = runBlocking {
 
 		val operations = listOf(
-			scope.async { sumOfInterpolation(10,7) },
-			scope.async { sumOfInterpolation(8,5) },
-			scope.async { sumOfInterpolation(6,3) },
-			scope.async { sumOfInterpolation(4,1) }
+			scope.async { sumofInterpolation(listOf(1, 3, 13)) },
+			scope.async { sumofInterpolation(listOf(1, 5, 17)) },
+			scope.async { sumofInterpolation(listOf(1, 7, 21)) },
+			scope.async { sumofInterpolation(listOf(1, 9, 25)) }
 		)
 
 		(operations.sumOf {
 			it.await()
 		} - 3) .toLong()/* -3 because all the sequences has 1 as element, we have to sum just once the number one */
 	}
+	private fun sumofInterpolation(givenValues: List<Int>): Int{
+		val fromValue = givenValues.size
+		var sum = givenValues.sum()
 
-	private fun sumOfInterpolation(b: Int, c: Int) =
-		(1..size).sumOf {
-			((4 * it.toDouble().pow(2)) - b * it + c).toInt()
+		var points = givenValues.mapIndexed { index, value ->
+			Pair(index, value)
 		}
+
+		for(current in fromValue..size){
+			sum += interpolate(points, current)
+		}
+
+		return sum
+	}
 }
