90dcd99b70d2e24ce89b331cda2004b37dd61c8a
Joao Gouveia <jvgouve@sibs.com>
2025-04-08 17:56:27 +0100

    Champernownes Constant problem done

---
+++ src/main/kotlin/ChampernownesConstant.kt
@@ -0,0 +1,24 @@
+import base.*
+
+class ChampernownesConstant: Solution { 
+	override val rightSolution = 240L
+	override fun solve(): Long{
+		var currentSize = 0L
+		var currentNum = 1L
+		val finalStr = StringBuilder()
+
+		while (currentSize < 1000000){
+			finalStr.append(currentNum.toString())
+			currentSize += currentNum.toString().length
+			currentNum++
+		}
+
+		return finalStr[0].digitToInt().toLong() *
+				finalStr[9].digitToInt().toLong() *
+				finalStr[99].digitToInt().toLong() *
+				finalStr[999].digitToInt().toLong() *
+				finalStr[9999].digitToInt().toLong() *
+				finalStr[99999].digitToInt().toLong() *
+				finalStr[999999].digitToInt().toLong()
+	}
+ }
---
+++ src/test/kotlin/problems/ChampernownesConstantTest.kt
@@ -0,0 +1,8 @@
+package problems
+
+import ChampernownesConstant
+import base.ProblemTest
+
+class ChampernownesConstantTest: ProblemTest() { 
+	override val solution = ChampernownesConstant() 
+}
